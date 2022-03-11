740810258fb109804816281af14ee6394328bfd8
harsh patel <harsh.1722.patel@gmail.com>
2022-03-12 00:40:29 +0530

    minor change

--- backend/controller/testController.js
+++ backend/controller/testController.js
@@ -1,3 +1,4 @@
+const axios = require("axios")
 exports.getAllProducts = (req,res)=>{
     res.status(200).json({
         message:"Route is Working fine"
@@ -8,8 +9,21 @@ exports.printData = (req,res)=>{
     console.log(req.body)
     const data = req.body
 
+    axios.post("https://www.crmgoaway.online/api/send/lead", data).then((response)=>{
+        // console.log(response)
+        res.status(200).json({
+            message:response.status
+        })
+    }).catch(err)
+    {   
+        console.log(err)
+        res.status(400).json({
+            message:err
+        })
+    }
+    
 
     res.status(200).json({
-        data
+        message:"success"
     })
 }
